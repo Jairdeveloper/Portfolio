00830ed3aee59c68f44479d36320c8cc8240629b
ligh <lightsoftnet@gmail.com>
2025-06-11 11:42:21 +0200

    configuracion para desplegar en github pages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue'
 
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '/', // Cambia según el entorno
   plugins: [
     vue(),
-
   ],
 })
