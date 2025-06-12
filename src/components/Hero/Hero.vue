e394b8febb4f2f1a1e3c5f8c03e005903f25b0d2
ligh <lightsoftnet@gmail.com>
2025-06-12 19:15:54 +0200

    Add: Funcionalidad a boton para descargar c.v

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -5,8 +5,8 @@
             <h1 class="text-4xl font-bold mb-4">Te doy la bienvenida a mi pagina web</h1>
             <p class="text-lg mb-8 py-6">Desarrollador Frontend ayudandote crear experiencias web únicas y mejorar tu presencia digital </p>
             <div class="flex flex-row items-center justify-start">
-                <a href="#portfolio" class="bg-lightgray text-dark px-6 py-3 shadow-lg hover:bg-graydark border border-dark transition duration-300">Saber más</a>
-                <a href="#portfolio" class="bg-lightgray text-dark px-6 py-3 shadow-lg hover:bg-graydark border border-dark transition duration-300 ml-12">Descargar CV</a>     
+                <a href="#about" class="bg-lightgray text-dark px-6 py-3 shadow-lg hover:bg-graydark border border-dark transition duration-300">Saber más</a>
+                <a href="Tech_Engineer_Resume_Template.pdf" download="cvJairFlores.pdf" class="bg-lightgray text-dark px-6 py-3 shadow-lg hover:bg-graydark border border-dark transition duration-300 ml-12">Descargar CV</a>     
             </div>
         </div>
         <div>
